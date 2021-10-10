8ea2e79fadfc14c9b3e972a2536b95e8688b21e6
Ali <ali.elesgerov@icloud.com>
2021-10-10 18:04:28 +0400

    first commit

---
+++ HomeWork2/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace HomeWork2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_run
+            // 
+            this.btn_run.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_run.Location = new System.Drawing.Point(258, 126);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(145, 42);
+            this.btn_run.TabIndex = 0;
+            this.btn_run.Text = "Press Me";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.MouseEnter += new System.EventHandler(this.btn_run_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_run;
+    }
+}
+
---
+++ HomeWork2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_run_MouseEnter(object sender, EventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                btn.Dispose();
+                Random random = new Random();
+                int rndX = random.Next(this.Width - 164);
+                int rndY = random.Next(this.Height - 52);
+                Button button = new Button
+                {
+                    Size = new Size(145, 42),
+                    Location = new Point(rndX, rndY),
+                    Text = "Press Me",
+                    Font = new Font("Microsoft Sans Serif", 12, FontStyle.Regular)
+                };
+                button.MouseEnter += btn_run_MouseEnter;
+                Controls.Add(button);
+            }
+        }
+    }
+}
